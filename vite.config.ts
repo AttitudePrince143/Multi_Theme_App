8538f1525bdbe5835c98c222e2726d912a2b57ed
AttitudePrince143 <sayyedadil22666@gmail.com>
2025-08-01 17:34:52 +0530

    modification

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Unified_Mentor/Multi-Theme-Switcher-App/', // 👈 must match subfolder path in repo
+  base: '/Multi_Theme_App/', // 👈 must match subfolder path in repo
   plugins: [react()],
 });
